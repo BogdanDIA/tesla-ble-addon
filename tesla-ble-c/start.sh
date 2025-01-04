1fa6d4d837e3e32855a4a696e3796ae5c086985c
BogdanDIA <b_diaconescu@yahoo.com>
2025-01-04 15:56:16 +0200

    Update the tesla-ble-addon submodule to use bashio

--- tesla-ble-c/start.sh
+++ tesla-ble-c/start.sh
@@ -1,4 +1,13 @@
-#!/bin/ash
+#!/bin/ash -e
 . /app/libproduct.sh
 
+log_info "Entering main loop..."
+
 exec /usr/sbin/sshd -D -e "$@"
+
+while :; do
+  log_info "This is ESP32 HCI Proxy"
+  sleep 30
+done
+
+
